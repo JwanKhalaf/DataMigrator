df6c5021906ced55ade5ad1dfe05b73c75aa23cc
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-01-16 23:59:56 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataMigrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
