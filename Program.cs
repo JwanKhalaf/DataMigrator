e2f3239eb747391cbb267cab6002c6a958b070fc
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-01-17 15:37:06 +0000

    added a web host

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace DataMigrator
 {
-    class Program
+  public class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      CreateHostBuilder(args)
+        .Build()
+        .Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
+  }
 }
